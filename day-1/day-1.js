6d8a45261f104a3a19d657227464a8a865cbea98
krushh77 <145192360+krushh77@users.noreply.github.com>
2023-10-27 00:11:53 +0530

    update

---
+++ day-1/day-1.js
@@ -0,0 +1,13 @@
+ console.log("krushnaraj");
+ console.log('krushnaraj');
+ console.log(`krushnaraj`);
+ console.log("12345");
+ console.log(`12345`);
+ console.log('1'+3);
+ console.log("1"+'2');
+ console.log("number daratype-");
+ console.log(1);
+ console.log(1+2);
+
+
+
