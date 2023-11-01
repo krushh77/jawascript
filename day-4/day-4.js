f605ceac6628dcd1ef74508f5ed611d8ec1c1722
krushh77 <145192360+krushh77@users.noreply.github.com>
2023-11-01 16:01:30 +0530

    update

---
+++ day-4/day-4.js
@@ -0,0 +1,23 @@
+var letters =["a","b","c","d"]
+
+letters.shift();
+letters.shift();
+letters.shift();
+console.log(letters)
+
+var numbers =[1,2,3,4,5,6,7,8,]
+numbers.unshift(77,99);
+numbers.unshift(66)
+console.log(numbers)
+
+for (var i = 1; i <= 10; i++)
+{console.log(i)}
+
+
+for (var i = 1; i < 10; i++)
+{console.log(i)}
+
+
+for (var i = 50 ; i<500; i += 50)
+{console.log(i)}
+
