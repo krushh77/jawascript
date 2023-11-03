31e7e104d0515f8008a23cb5db8fd2a35bc59296
krushh77 <145192360+krushh77@users.noreply.github.com>
2023-11-03 11:15:07 +0530

    updates

---
+++ day-5/day-5.js
@@ -0,0 +1,38 @@
+// for (var i =10; i>0; i-- ){
+//     console.log(i)
+// }
+
+// for ( i = 750; i >= 350; i -= 5){
+//     console.log(i)
+// }
+
+// for (var i = 50; i >= 10; i-=2){
+//     console.log(i)
+// }
+
+// for (var i = 10; i > 0; i--){
+//     console.log(i)
+// }
+
+// for (var i = 30; i > 0; i--){
+//     console.log(i)
+// }
+
+// var students =["rohit" , "virat" , "rahul" , "surya"];
+
+// for (var i = 0; i< students.length; i++){
+//     console.log(students[i],i)
+// }
+
+
+var numbers = [10,20,30,40,50,60]
+for (var i = numbers.length - 2; i >= 0 ; i-=10){
+   console.log(numbers[i])
+}
+
+
+var numbers = [1,2,3,4,5,6]
+
+for (var i = 10; i>1 ; i-=5){
+    console.log(numbers[i])
+}
