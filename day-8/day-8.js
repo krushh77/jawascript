5aff549b3b5e8ff06f7a0b223f80a7a551755f6f
krushh77 <145192360+krushh77@users.noreply.github.com>
2023-11-11 10:37:23 +0530

    update

---
+++ day-8/day-8.js
@@ -0,0 +1,80 @@
+// function addition(){
+//     var number1 = 10;
+//     var number2 = 20;
+//     console.log(number1 + number2)
+// }
+// addition();
+
+// function addition(num1,num2,num3){
+//     console.log(num1,num2,num3)
+// }
+// addition(10,20);
+
+// function addition(num1,num2,num3){
+//     console.log(num1,num2,num3)
+// }
+// addition(10,30,20);
+
+// function addition(num1,num2){
+//     console.log(num1 + num2)
+// }
+
+// function addition(num1,num2){
+//     console.log(num1+num2)
+// }
+// addition(30,40);
+
+// var array =[1,2,3,4,5,6,7,8]
+// var target = 8;
+// function findtarget(target,array){
+//     var flag = false
+//     for (var i =0; i < array.length; i++) {
+//     if(array[i]==target) {
+//         console.log("we got it")
+//         flag = true;
+//     }
+
+//     }
+//     if (flag == false){
+//         console.log("not found.")
+//     }
+
+// }
+// findtarget(target,array)
+
+// function functionone(){
+//     console.log("first")
+//     console.log("second")
+//     return "hii from function"
+// }
+// functionone()
+
+// function addition(num1,num2){
+//     return num1 + num2
+// }
+// var answer = addition(2,3)
+// console.log(answer)
+
+
+// function findeven(number){
+//     if (number % 2 == 0){
+//         return "number is even"
+//     }else{
+//         return  "number is odd"
+//         console.log("hii")
+//     }
+// }
+// console.log(findeven(3))
+
+function find(number){
+    if (number % 2 == 0){
+        return "number is even"
+    }
+    console.log("hii")
+    if (number > 10){
+        return "number is greater than 10"
+    }
+}
+console.log(find(14))
+
+
